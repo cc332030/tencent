c7167b04f66cf0023eb37e6ebf2827235c12c080
c332030 <c332030@c332030.com>
2023-06-30 17:28:07 +0800

    feat: cloudbase-pipeline-trigger.yml

---
+++ cloudbase/pipeline-trigger.sh
@@ -0,0 +1,40 @@
+
+curl -u ${USERNAME}:${PASSWORD} \
+   -v -X POST "${URL}" \
+   -H 'Content-Type: application/json' \
+   -d "
+    {
+    \"ref\": \"master\",
+    \"envs\": [
+        {
+            \"name\": \"TCR_REGISTRY_CREDENTIAL\",
+            \"value\": \"${TCR_REGISTRY_CREDENTIAL}\",
+            \"sensitive\": 0
+        },
+        {
+            \"name\": \"TCR_REGISTRY_HOSTNAME\",
+            \"value\": \"ccr.ccs.tencentyun.com\",
+            \"sensitive\": 0
+        },
+        {
+            \"name\": \"TCR_NAMESPACE_NAME\",
+            \"value\": \"${TCR_NAMESPACE_NAME}\",
+            \"sensitive\": 0
+        },
+        {
+            \"name\": \"OWNER\",
+            \"value\": \"${OWNER}\",
+            \"sensitive\": 0
+        },
+        {
+            \"name\": \"REPOSITORY_NAME\",
+            \"value\": \"${REPOSITORY_NAME}\",
+            \"sensitive\": 0
+        },
+        {
+            \"name\": \"BRANCH\",
+            \"value\": \"${BRANCH}\",
+            \"sensitive\": 0
+        }
+    ]
+}"
